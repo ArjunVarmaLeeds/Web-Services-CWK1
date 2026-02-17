80e621e65ed17ac01610183cd5289fa1b7e72279
ArjunVarmaLeeds <sc22a2v@leeds.ac.uk>
2026-02-17 14:16:06 +0000

    Fixed env.js file code

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -6,26 +6,12 @@ dotenv.config();
 const cleaned = cleanEnv(process.env, {
     NODE_ENV: str({ default: "development" }),
     PORT: port({ default: 5000 }),
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-
-=======
->>>>>>> 43cc98f0f60c79628e8014d28c9bec2f1fc518b1
-    DATABASE_URL: str(),
-    JWT_SECRET: str(),
-    RIOT_API_KEY: str(),
-    RIOT_REGION: str({ default: "europe" }),
-});
-<<<<<<< HEAD
-=======
     DATABASE_URL: str(),
     JWT_SECRET: str(),
     RIOT_API_KEY: str(),
     RIOT_REGION: str({ default: "europe" }),
     BASE_URL: str({ default: "https://europe.api.riotgames.com" })
 });
-=======
->>>>>>> 43cc98f0f60c79628e8014d28c9bec2f1fc518b1
 
 export const env = {
     nodeEnv: cleaned.NODE_ENV,
@@ -38,12 +24,6 @@ export const env = {
     riot: {
         apiKey: cleaned.RIOT_API_KEY,
         region: cleaned.RIOT_REGION,
-<<<<<<< HEAD
         baseUrl: cleaned.BASE_URL.replace('region', cleaned.RIOT_REGION)
     },
 };
->>>>>>> Stashed changes
-=======
-    },
-};
->>>>>>> 43cc98f0f60c79628e8014d28c9bec2f1fc518b1
